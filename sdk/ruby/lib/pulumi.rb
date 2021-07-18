3189a54adf81ab6ff87a9203bedf376b8969beee
Wenxuan Zhao <viz@linux.com>
2021-07-18 18:31:21 +0800

    WIP

---
+++ sdk/ruby/lib/pulumi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pulumi/version"
+
+module Pulumi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ sdk/ruby/lib/pulumi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Pulumi
+  VERSION = "0.1.0"
+end
